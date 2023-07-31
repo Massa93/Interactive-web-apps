7a045d3c8d714486ea4dec857358617f73cca142
Sazi Massa <sazimashele@gmail.com>
2023-07-31 17:16:30 +0200

    completed

---
+++ IWA1/scripts.js
@@ -0,0 +1,34 @@
+const MAX_NUMBER = 14
+const MIN_NUMBER = -5
+
+
+const number= document.querySelector('[da ta-key="number"]')
+const subtract= document.querySelector('[data-key="subtract"]')
+const add= document.querySelector('[data-key="add"]')
+
+const substractHandler= () =>{
+    const newValue = parseInt(number.value)-1
+    number.value= newValue;  
+
+    if( number.value.disabled===true){
+        add.disabled=false           
+        }
+        else{
+            subtract.disabled=false
+        }
+}
+const addHandler= () =>{
+    const newValue=parseInt(number.value)-1
+    number.value=number.value+1;
+
+    if(subtract.disabled===true){
+    add.disabled = true
+    }
+    else{
+        add.disabled=false
+    }
+
+}
+
+subtract.addEventListener('click', substractHandler)
+add.addEventListener('click', addHandler)
